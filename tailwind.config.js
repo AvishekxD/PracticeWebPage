1364ec69102265dcdd8e6a3e0664770bbb7b70e2
AvishekxD <abmeena00@gmail.com>
2024-09-09 19:52:36 +0530

    adding files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        NeueRegular:['Neue-regular', 'Sans-serif'],
+        NeueLight : ['Neue-light', 'Sans-serif'],
+        NeueBold : ['Neue-bold', 'Sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
